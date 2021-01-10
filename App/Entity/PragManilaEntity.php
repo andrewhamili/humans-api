5a7d978890d2da8c3b9c0b4bedb1cd0b5ac6d5a3
andrewhamili <rockr1521@gmail.com>
2021-01-10 21:53:24 +0800

    Added PUT method for updating Human record by ID

--- App/Entity/PragManilaEntity.php
+++ App/Entity/PragManilaEntity.php
@@ -69,6 +69,50 @@
                 die();
             }
         }
+        function Human_delete(int $id){
+            $stmt = $this->conn->prepare('CALL Human_delete(?)');
+
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
+
+            $stmt->execute();
+            if($stmt->errorCode()=='00000'){
+                return $stmt->fetchAll();
+            }else if($stmt->errorCode()=='42000'){
+                http_response_code(400);
+                $array = array('message'=>$stmt->errorInfo());
+                error_log(json_encode($array));
+                echo json_encode($array);
+                die();
+            }else{
+                http_response_code(500);
+                $array = array('message'=>'An internal server error occured.');
+                error_log(json_encode($stmt->errorInfo()));
+                die();
+            }
+        }
+        function Human_updateById(int $id, string $firstName, string $lastName){
+            $stmt = $this->conn->prepare('CALL Human_updateById(?,?,?)');
+
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
+            $stmt->bindParam(2, $firstName);
+            $stmt->bindParam(3, $lastName);
+
+            $stmt->execute();
+            if($stmt->errorCode()=='00000'){
+                return $stmt->fetchAll();
+            }else if($stmt->errorCode()=='42000'){
+                http_response_code(400);
+                $array = array('message'=>$stmt->errorInfo());
+                error_log(json_encode($array));
+                echo json_encode($array);
+                die();
+            }else{
+                http_response_code(500);
+                $array = array('message'=>'An internal server error occured.');
+                error_log(json_encode($stmt->errorInfo()));
+                die();
+            }
+        }
     }
 
 ?>
--- human/index.php
+++ human/index.php
@@ -42,6 +42,37 @@
                 $apiResponse->badRequest(array('message'=>'Invalid parameters passed'));
             }
         }
+    }else if($method=='DELETE'){
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+            $db->Human_delete($id);
+            $apiResponse->ok(array());
+        }else{
+            $apiResponse->badRequest(array('message'=>'Invalid parameters passed'));
+        }
+    }else if ($method=='PUT'){
+        $requestBody = file_get_contents('php://input');
+        
+        $validatorResult = $validator->validateJson($requestBody);
+
+        if($validatorResult['invalid']){
+            $apiResponse->badRequest(array('message'=>$validatorResult['error']));
+        }else{
+            $requestObject = $validatorResult['result'];
+
+            if(array_key_exists('id', $requestObject) && array_key_exists('first_name', $requestObject) && array_key_exists('last_name', $requestObject)){
+
+                if(empty($requestObject['id']) && empty($requestObject['first_name']) && empty($requestObject['last_name'])){
+                    $apiResponse->badRequest(array('message'=>'Invalid parameters passed'));    
+                }else{
+
+                    $db->Human_updateById($requestObject['id'], $requestObject['first_name'], $requestObject['last_name']);
+                    $apiResponse->ok(array());
+                }
+            }else{
+                $apiResponse->badRequest(array('message'=>'Invalid parameters passed'));
+            }
+        }
     }else{
         $apiResponse->methodNotAllowed(array());
     }
